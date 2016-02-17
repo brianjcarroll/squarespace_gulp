7bdceafc47a2a9b9c717717c0c206e1c8ef9aec4
Brian Carroll <brianjcarroll0@gmail.com>
2016-02-17 16:56:24 -0500

    sftp auth file config

---
+++ gulp-sftp-auth.js
@@ -0,0 +1,9 @@
+const auth = {
+  host: 'dev.squarespace.com',
+  user: '', /* Email address */
+  pass: '', /* Password */
+  port: 2030,
+  remotePath: '', /*   Website identifier in slashes. EX: /bcarroll/   */
+};
+
+export default auth;
